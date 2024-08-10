dc1ef4e0f393663cda9cec81f175cf9e9a0429c3
Deepakverma2102 <veer811514@gmail.com>
2024-08-10 13:33:12 +0530

    Add files via upload

---
+++ healthy-living/scripts.js
@@ -0,0 +1,5 @@
+// Add any JavaScript you need here
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us!');
+});
